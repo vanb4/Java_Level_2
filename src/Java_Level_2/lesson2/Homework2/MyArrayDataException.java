732bbec5a6ceaa7e0be78ba3bd49f26396c2ae95
vanb4 <vanvanb4@gmail.com>
2020-12-12 18:43:11 +0400

    comit

--- src/Java_Level_2/lesson2/Homework2/MyArrayDataException.java
+++ src/Java_Level_2/lesson2/Homework2/MyArrayDataException.java
@@ -1,4 +1,19 @@
 package Java_Level_2.lesson2.Homework2;
 
-public class MyArrayDataException {
+public class MyArrayDataException extends MyException{
+    private final int x;
+    private final int y;
+
+    public MyArrayDataException(int x, int y) {
+        super("Heверный формат данных");
+        this.x = x;
+        this.y = y;
+    }
+
+
+
+    public String getFullMessage() {
+        return this.getMessage() + ":ячейка массива [" + x + "," + y +"]";
+    }
+
 }
